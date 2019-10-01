fa47f1d60e24a02953bd1b8718ae7382e941caf9
Ben Venker <bvemails@gmail.com>
2019-10-01 09:44:40 -0700

    implement advanced routing

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import CollectionItem from '../../components/collection-item/collection-item.component';
+
+import { selectCollection } from '../../redux/shop/shop.selectors';
+
+import './collection.styles.scss';
+import { connect } from 'react-redux';
+
+
+const CollectionPage = ({ collection }) => {
+
+  return (
+    <div className="collection-page">
+      <h2>COLLECTION PAGE</h2>
+    </div>
+  )
+};
+
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
+
+export default connect(mapStateToProps)(CollectionPage);
+
+
+
